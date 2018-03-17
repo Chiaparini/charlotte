32cdd581f23eaa92a1b63b507078d20e8ac3542a
Lucas Chiaparini <chiaparini@Lucass-MacBook-Pro.local>
2018-03-17 20:29:08 -0300

    first of all

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+	<div id="app">
+		<!-- Header -->
+		<!-- Main -->
+		<!-- Footer -->
+	</div>
+</template>
+
+<script>
+	import Main from '_app/views/Main.vue'
+	import Header from '_app/views/templates/HeaderTemplate.vue'
+	import Footer from '_app/views/templates/FooterTemplate.vue'
+
+	export default {
+		name: 'app',
+
+		components: {
+			Main,
+			Header,
+			Footer
+		},
+
+		data () {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '_scss/_main.scss'
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+	el: '#app',
+	render: h => h(App)
+})
